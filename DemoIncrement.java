0c67bdabe08703844eaf9178a61a81165d41cc28
os492 <os492@exeter.ac.uk>
2026-02-05 18:17:46 +0000

    DemoIncrement Completed

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -5,11 +5,16 @@ public class DemoIncrement {
         int b = a++ + 5;
         int x = 3;
         int y = ++x + 5;
+        int p = 3;
+        p++;
+        int q = p + 5; 
 
         System.out.print("a: " + a);
         System.out.print(", b: " + b);
         System.out.print(", x: " + x);
-        System.out.println(", y: " + y);
+        System.out.print(", y: " + y);
+        System.out.print(", p: " + p);
+        System.out.println(", q: " + q);
 
         // Replace the xxxxx in the following with the correct term
         System.out.println("i++ means post-increment");
